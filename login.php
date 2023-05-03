6bbd17f6cc0b6f6a5d2b284e6e88b0721899fd49
mariiakrasniuk <mariia.pirogova@gmail.com>
2023-05-03 09:25:11 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,30 @@
+<?php 
+$is_invalid = false;
+
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+    $mysqli = new mysqli('localhost', 'itech174', 'Fe7@bwZWgAqV', 'itech174');
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+    }
+    $sql = sprintf("SELECT * FROM users 
+    WHERE email='%s'", 
+    $mysqli->real_escape_string($_POST["email"]));
+    
+    $result = $mysqli->query($sql);
+    $user = $result->fetch_assoc();
+    if($user) {
+        if(password_verify($_POST["password"], $user["password_hash"])){
+            session_start();
+            $_SESSION["user_id"] = $user["id"];
+            header("Location: index.php");
+            exit;
+        }else {
+            $is_invalid = true;
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,33 +39,37 @@
 </head>
 
 <body>
-    <?php
-        if($_COOKIE['user'] == ''):
-    ?>
     <div class="container">
         <div class="title">Login</div>
-        <form action="authorization.php">
+        <?php if($is_invalid): ?>
+            <em>Invalid login</em>
+        <?php endif; ?>
+        <form method="post">
             <div class="user-details">
                 <div class="input-box">
                     <span class="details">Email Address</span>
-                    <input type="text" placeholder="Enter your email" required>
+                    <input type="text" name="email" 
+                    value="<?= htmlspecialchars($_POST["email"] ?? "") ?>" 
+                    placeholder="Enter your email">
                 </div>
                 <div class="input-box">
                     <span class="details">Password</span>
-                    <input type="password" placeholder="Enter your password" required>
+                    <input type="password" name="password" placeholder="Enter your password">
                 </div>
             </div>
             <div class="text">
                 Don't have an account? <a class="signup" href="signup.html">Sign Up</a>
             </div>
+            <div class="text">
+                <a class="signup" href="password-reset.html">Reset password</a>
+            </div>
+            <div class="text">
+                <a class="signup" href="delete-account.html">Delete your account</a>
+            </div>
             <div class="button">
                 <input type="submit" value="Login">
             </div>
         </form>
-        <?php else: ?>
-            <p>You are authorized! To go to main page press <a href="/exit.php">here</a>.</p>
-
-        <?php endif;?>
     </div>
 </body>
 
