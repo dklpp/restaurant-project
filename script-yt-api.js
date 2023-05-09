9a04a65916dad00d511b76f5cf28e1850b9873f2
Dmytro Klepachevskyi <74605425+dklpp@users.noreply.github.com>
2023-05-09 21:12:13 +0300

    Update script-yt-api.js
    Updated Youtube API to display videos on a webpage

--- script-yt-api.js
+++ script-yt-api.js
@@ -1,5 +1,25 @@
+// let nextPageToken = ""
+//     fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCBapjRd-uyeCTGb_-AMEqYw&maxResults=10&order=date&key=AIzaSyBTVtGsV6qrsxM_h-ZQwR8ksvkaPnujl0Y&pageToken="+nextPageToken)
+//     .then((result)=>{
+//         return result.json()
+//     }).then((data)=>{
+//         console.log(data)
+//         let videos = data.items
+//         nextPageToken = data.nextPageToken
+//         let videoContainer = document.querySelector(".youtube-container")
+//         for(video of videos) {
+//             videoContainer.innerHTML += `
+//                 <h3>${video.snippet.title}</h3>
+//                 <img src="${video.snippet.thumbnails.high.url}">
+//             `
+//         }
+//     })
+
+
+
+
 let nextPageToken = ""
-    fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCBapjRd-uyeCTGb_-AMEqYw&maxResults=10&order=date&key=AIzaSyBTVtGsV6qrsxM_h-ZQwR8ksvkaPnujl0Y&pageToken="+nextPageToken)
+fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCBapjRd-uyeCTGb_-AMEqYw&maxResults=10&order=date&key=AIzaSyBTVtGsV6qrsxM_h-ZQwR8ksvkaPnujl0Y&pageToken="+nextPageToken)
     .then((result)=>{
         return result.json()
     }).then((data)=>{
@@ -10,7 +30,8 @@ let nextPageToken = ""
         for(video of videos) {
             videoContainer.innerHTML += `
                 <h3>${video.snippet.title}</h3>
-                <img src="${video.snippet.thumbnails.high.url}">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/${video.id.videoId}" frameborder="0" allowfullscreen></iframe>
             `
         }
     })
+    
